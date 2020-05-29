a1f5426b3ec93d0a3b134132da4b98467e34d541
root <pavel.vasev@gmail.com>
2020-05-29 12:00:24 +0500

    better warning on no state dir

--- src.v1/z1_10_main.rb
+++ src.v1/z1_10_main.rb
@@ -25,19 +25,19 @@ module DasMain
   end
 
   def to_s
-    "zapusk: dir=#{self.dir}, cmd=#{self.cmd}, name=#{self.name}, state_dir=#{self.state_dir}"
+    "zapusk: zdb_dir=#{self.dir}, cmd=#{self.cmd}, name=#{self.name}, state_dir=#{self.state_dir}"
   end
 
   def ready?
     if comps.length == 0
       warning "ready?: empty components list! this is not error, just warning!"
-      info    "ready?: dir=#{self.dir}"
+      info    "ready?: zdb_dir=#{self.dir}"
     end
-    raise "ready?: zdb dir file test failed, is not a directory! dir=#{dir}" if !File.directory?(dir)
+    raise "ready?: zdb dir file test failed, is not a directory! zfb_dir=#{dir}" if !File.directory?(dir)
     raise "ready?: components not loaded!" if comps.nil?
     raise "ready?: cmd in invalid format! cmd=#{self.cmd}" if self.cmd.nil? || self.cmd !~ /^[\w_-]+$/ # multiline??
     raise "ready?: name is not assigned!" if self.name.nil?
-    raise "ready?: state_dir is not assigned!" if self.state_dir.nil?
+    raise "ready?: state_dir is not assigned! Are your running zapusk program?" if self.state_dir.nil?
     raise "ready?: state_dir too short!" if File.expand_path(self.state_dir).length < 4
     if comps.length > 0 # пусть отсутствие глоб имени будет ошибкой только при наличии компонент
       raise "ready?: global_name length is 0!" if self.global_name.length == 0 
