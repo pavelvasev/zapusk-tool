32003f7e2196e83452efc07449fa399bff316e57
Pavel Vasev <pavel.vasev@gmail.com>
2020-06-24 18:57:02 +0500

    bugfix

--- src.v1/z1_28_params_insert.rb
+++ src.v1/z1_28_params_insert.rb
@@ -8,7 +8,9 @@ module DasParamsComputeInsert
     if params["params"]
       log "ok i see params in params. btw params=#{params}"
       params_of_current_zdb = self.params
-      other_trash = {"hilevel"=>true, "_component_name"=>"papapdf", "type"=>"papapdf", "parent_global_name"=>"vasev", "component_global_name"=>"vasev-papapdf", "stack_of_types_dirs"=>"/zapusk/vasev.zdb"}
+      other_trash = {"hilevel"=>true, "_component_name"=>"papapdf", "type"=>"papapdf", "parent_global_name"=>"vasev", 
+                     "component_global_name"=>"vasev-papapdf", "stack_of_types_dirs"=>"/zapusk/vasev.zdb",
+                     "type_dir" => "xxx"}
       trash_names = global_conf.merge( zapusk_params ).merge( other_trash )
       a = params_of_current_zdb.delete_if{ |k,v| trash_names.has_key?(k) }
       log "%params% keyword magic: inserting this things: #{a.inspect}"
