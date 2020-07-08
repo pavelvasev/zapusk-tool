5983febba98d1bfdb7cf28d15f6e007229056bbe
Pavel Vasev <pavel.vasev@gmail.com>
2020-07-08 06:34:29 +0000

    zapusk default params

---
+++ src.v1/z1_77_zapusk_defaults_conf.rb
@@ -0,0 +1,23 @@
+# aim: load zapusk.default.conf
+
+# effect: merge it within global_conf provided by zapusk_global_conf
+
+module DasZapuskConfDefault
+
+  attr_accessor :global_conf
+  
+  def load_global_conf
+    super
+    
+    fn = File.join( Zapusk::TOOL_DIR,"zapusk.tool-defaults.conf" )
+    if File.exist?(fn)
+      defaults = read_params_file( fn )
+      
+      self.global_conf = defaults.merge( self.global_conf )
+    end
+
+  end
+
+end
+
+Zapusk.prepend DasZapuskConfDefault
